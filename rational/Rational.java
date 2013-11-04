e0c34090ab7b4560c1e322fe0eba488f7819daca
B Kleinen <kleinen@infrastructure.de>
2013-11-04 22:44:37 +0100

    inital commit

---
+++ rational/Rational.java
@@ -0,0 +1,22 @@
+
+/**
+ * Represents a Rational Number.
+ * 
+ * @author Barbara Kleinen
+ * @version 04.11.2013
+ */
+public class Rational
+{
+    private int numerator;
+    private int denominator;
+
+    /**
+     * Constructor for objects of class Rational
+     */
+    public Rational(int numerator,int denominator)
+    {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+}
