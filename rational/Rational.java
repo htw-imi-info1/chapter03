4843522899750edd929732b44a899be6e8ed7776
B Kleinen <kleinen@infrastructure.de>
2014-11-11 18:40:49 +0100

    removed this

--- rational/Rational.java
+++ rational/Rational.java
@@ -15,17 +15,17 @@ public class Rational
      */
     public Rational(int numerator,int denominator)
     {
-        this.num = numerator;
-        this.den = denominator;
+        num = numerator;
+        den = denominator;
     }
 
     public Rational add(Rational r){
         return new Rational(
-            this.num*r.den + r.num*this.den,
-            this.den*r.den);
+            num*r.den + r.num*den,
+            den*r.den);
     }
     public String toString(){
-        return ""+ num+"/" + den;
+        return ""+ num + "/" + den;
     }
 
 }
