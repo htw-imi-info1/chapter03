96e0bdf14abe0f8ee7b8a4eb870b6451cc94f785
B Kleinen <kleinen@infrastructure.de>
2019-11-04 19:36:48 +0100

    added update thread

--- clock-display-with-GUI/Clock.java
+++ clock-display-with-GUI/Clock.java
@@ -7,7 +7,7 @@ import javax.swing.border.*;
  * A very simple GUI (graphical user interface) for the clock display.
  * In this implementation, time runs at about 3 minutes per second, so that
  * testing the display is a little quicker.
- * 
+ *
  * @author Michael Kölling and David J. Barnes
  * @version 2016.02.29
  */
@@ -18,20 +18,25 @@ public class Clock
     private ClockDisplay clockDisplay;
     private boolean clockRunning = false;
     private TimerThread timerThread;
-    
+    private UpdateThread updater;
+
     /**
      * Constructor for objects of class Clock
      */
     public Clock()
     {
+        clockDisplay = new ClockDisplay(12,0);
         makeFrame();
-        clockDisplay = new ClockDisplay();
+        updater = new UpdateThread();
+        updater.start();
     }
+
     public ClockDisplay getClockDisplay(){
         return clockDisplay;
     }
+
     /**
-     * 
+     *
      */
     private void start()
     {
@@ -39,36 +44,44 @@ public class Clock
         timerThread = new TimerThread();
         timerThread.start();
     }
-    
+
     /**
-     * 
+     *
      */
     private void stop()
     {
         clockRunning = false;
     }
-    
+
     /**
-     * 
+     *
+     */
+    private void update()
+    {
+        label.setText(clockDisplay.getTime());
+    }
+
+    /**
+     *
      */
     private void step()
     {
         clockDisplay.timeTick();
         label.setText(clockDisplay.getTime());
     }
-    
+
     /**
      * 'About' function: show the 'about' box.
      */
     private void showAbout()
     {
-        JOptionPane.showMessageDialog (frame, 
-                    "Clock Version 1.0\n" +
-                    "A simple interface for the 'Objects First' clock display project",
-                    "About Clock", 
-                    JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog (frame,
+            "Clock Version 1.0\n" +
+            "A simple interface for the 'Objects First' clock display project",
+            "About Clock",
+            JOptionPane.INFORMATION_MESSAGE);
     }
-    
+
     /**
      * Quit function: quit the application.
      */
@@ -77,7 +90,6 @@ public class Clock
         System.exit(0);
     }
 
-    
     /**
      * Create the Swing frame and its content.
      */
@@ -88,12 +100,12 @@ public class Clock
         contentPane.setBorder(new EmptyBorder(1, 60, 1, 60));
 
         makeMenuBar(frame);
-        
+
         // Specify the layout manager with nice spacing
         contentPane.setLayout(new BorderLayout(12, 12));
-        
+
         // Create the image pane in the center
-        label = new JLabel("00:00", SwingConstants.CENTER);
+        label = new JLabel(clockDisplay.getTime(), SwingConstants.CENTER);
         Font displayFont = label.getFont().deriveFont(96.0f);
         label.setFont(displayFont);
         //imagePanel.setBorder(new EtchedBorder());
@@ -102,14 +114,18 @@ public class Clock
         // Create the toolbar with the buttons
         JPanel toolbar = new JPanel();
         toolbar.setLayout(new GridLayout(1, 0));
-        
+
         JButton startButton = new JButton("Start");
         startButton.addActionListener(e -> start());
         toolbar.add(startButton);
-        
+
         JButton stopButton = new JButton("Stop");
         stopButton.addActionListener(e -> stop());
         toolbar.add(stopButton);
+
+        JButton updateButton = new JButton("Update");
+        updateButton.addActionListener(e -> update());
+        toolbar.add(updateButton);
 
         JButton stepButton = new JButton("Step");
         stepButton.addActionListener(e -> step());
@@ -118,21 +134,21 @@ public class Clock
         // Add toolbar into panel with flow layout for spacing
         JPanel flow = new JPanel();
         flow.add(toolbar);
-        
+
         contentPane.add(flow, BorderLayout.SOUTH);
-        
-        // building is done - arrange the components      
+
+        // building is done - arrange the components
         frame.pack();
-        
+
         // place the frame at the center of the screen and show
         Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setLocation(d.width/2 - frame.getWidth()/2, d.height/2 - frame.getHeight()/2);
         frame.setVisible(true);
     }
-    
+
     /**
      * Create the main frame's menu bar.
-     * 
+     *
      * @param frame   The frame that the menu bar should be added to.
      */
     private void makeMenuBar(JFrame frame)
@@ -142,37 +158,27 @@ public class Clock
 
         JMenuBar menubar = new JMenuBar();
         frame.setJMenuBar(menubar);
-        
+
         JMenu menu;
         JMenuItem item;
-        
+
         // create the File menu
         menu = new JMenu("File");
         menubar.add(menu);
-        
+
         item = new JMenuItem("About Clock...");
-            item.addActionListener(e -> showAbout());
+        item.addActionListener(e -> showAbout());
         menu.add(item);
 
         menu.addSeparator();
-        
+
         item = new JMenuItem("Quit");
-            item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, SHORTCUT_MASK));
-            item.addActionListener(e -> quit());
+        item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, SHORTCUT_MASK));
+        item.addActionListener(e -> quit());
         menu.add(item);
     }
-    
-    class TimerThread extends Thread
-    {
-        public void run()
-        {
-            while (clockRunning) {
-                step();
-                pause();
-            }
-        }
-        
-        private void pause()
+    abstract class LocalThread extends Thread{
+        protected void pause()
         {
             try {
                 Thread.sleep(300);   // pause for 300 milliseconds
@@ -181,4 +187,27 @@ public class Clock
             }
         }
     }
+    class TimerThread extends LocalThread
+    {
+        public void run()
+        {
+            while (clockRunning) {
+                step();
+                pause();
+            }
+        }
+
+    }
+    class UpdateThread extends LocalThread
+    {
+        public void run()
+        {
+            while (true) {
+                update();
+                pause();
+            }
+        }
+
+    }
+
 }
